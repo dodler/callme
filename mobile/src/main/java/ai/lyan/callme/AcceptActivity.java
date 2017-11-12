aa33e43d92140266010adc0206fb9e454e79ab33
artyom_lyan <avn8068@gmail.com>
2017-11-12 19:54:31 +0300

    qr code added

--- mobile/src/main/java/ai/lyan/callme/AcceptActivity.java
+++ mobile/src/main/java/ai/lyan/callme/AcceptActivity.java
@@ -1,10 +1,31 @@
 package ai.lyan.callme;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.SparseArray;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 import ai.lyan.callme.contact.Conctact;
 
@@ -13,13 +34,30 @@ import ai.lyan.callme.contact.Conctact;
  */
 
 public class AcceptActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = AcceptActivity.class.toString();
+
+    private static final int PHOTO_REQUEST = 10;
+    private static final int REQUEST_WRITE_PERMISSION = 20;
+
+    private BarcodeDetector detector;
+    private Uri imageUri;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accept_activity);
 
-        Intent intent = new Intent(getApplicationContext(), BarcodeCaptureActivity.class);
-        startActivityForResult(intent, 1);
+        detector = new BarcodeDetector.Builder(getApplicationContext())
+                .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                .build();
+        if (!detector.isOperational()) {
+            Log.e(LOG_TAG, "not operational detactor");
+            return;
+        }
+
+        ActivityCompat.requestPermissions(AcceptActivity.this, new
+                String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
     }
 
 
@@ -30,5 +68,122 @@ public class AcceptActivity extends AppCompatActivity {
         conctact.acceptIntent(contactIntent);
 
         startActivityForResult(contactIntent, 1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_WRITE_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    takePicture();
+                } else {
+                    Toast.makeText(AcceptActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    private void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photo = new File(Environment.getExternalStorageDirectory(), "picture.jpg");
+        imageUri = FileProvider.getUriForFile(AcceptActivity.this,
+                BuildConfig.APPLICATION_ID + ".provider", photo);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, PHOTO_REQUEST);
+    }
+
+    private Bitmap decodeBitmapUri(Context ctx, Uri uri) throws FileNotFoundException {
+        int targetW = 600;
+        int targetH = 600;
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(ctx.getContentResolver().openInputStream(uri), null, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        return BitmapFactory.decodeStream(ctx.getContentResolver()
+                .openInputStream(uri), null, bmOptions);
+    }
+
+    private void launchMediaScanIntent() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        mediaScanIntent.setData(imageUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PHOTO_REQUEST && resultCode == RESULT_OK) {
+            launchMediaScanIntent();
+            try {
+                Bitmap bitmap = decodeBitmapUri(this, imageUri);
+                if (detector.isOperational() && bitmap != null) {
+                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                    SparseArray<Barcode> barcodes = detector.detect(frame);
+                    for (int index = 0; index < barcodes.size(); index++) {
+                        Barcode code = barcodes.valueAt(index);
+
+                        Log.i(LOG_TAG, code.displayValue);
+
+                        //Required only if you need to extract the type of barcode
+                        int type = barcodes.valueAt(index).valueFormat;
+                        switch (type) {
+                            case Barcode.CONTACT_INFO:
+                                Log.i(LOG_TAG, code.contactInfo.title);
+                                break;
+                            case Barcode.EMAIL:
+                                Log.i(LOG_TAG, code.email.address);
+                                break;
+                            case Barcode.ISBN:
+                                Log.i(LOG_TAG, code.rawValue);
+                                break;
+                            case Barcode.PHONE:
+                                Log.i(LOG_TAG, code.phone.number);
+                                break;
+                            case Barcode.PRODUCT:
+                                Log.i(LOG_TAG, code.rawValue);
+                                break;
+                            case Barcode.SMS:
+                                Log.i(LOG_TAG, code.sms.message);
+                                break;
+                            case Barcode.TEXT:
+                                Log.i(LOG_TAG, code.rawValue);
+                                break;
+                            case Barcode.URL:
+                                Log.i(LOG_TAG, "url: " + code.url.url);
+                                break;
+                            case Barcode.WIFI:
+                                Log.i(LOG_TAG, code.wifi.ssid);
+                                break;
+                            case Barcode.GEO:
+                                Log.i(LOG_TAG, code.geoPoint.lat + ":" + code.geoPoint.lng);
+                                break;
+                            case Barcode.CALENDAR_EVENT:
+                                Log.i(LOG_TAG, code.calendarEvent.description);
+                                break;
+                            case Barcode.DRIVER_LICENSE:
+                                Log.i(LOG_TAG, code.driverLicense.licenseNumber);
+                                break;
+                            default:
+                                Log.i(LOG_TAG, code.rawValue);
+                                break;
+                        }
+                    }
+                    if (barcodes.size() == 0) {
+                        Log.e(LOG_TAG, "Scan Failed: Found nothing to scan");
+                    }
+                } else {
+                    Log.e(LOG_TAG, "Could not set up the detector!");
+                }
+            } catch (Exception e) {
+                Toast.makeText(this, "Failed to load Image", Toast.LENGTH_SHORT)
+                        .show();
+                Log.e(LOG_TAG, e.toString());
+            }
+        }
     }
 }
